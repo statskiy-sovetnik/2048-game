5ab705bb928883c9871141b78e8ad5c4caaa4bf5
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-08-20 12:12:16 +0500

    (feat): Added React + JSX and ES6 modules support
    Added React and React-dom
    Configured gulp+babelify+browserify to support
    ES6 raw paths and other stuff for modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      prefix = require('gulp-autoprefixer'),
+      browserify = require('browserify'),
+      babelify = require('babelify'),
+      source = require('vinyl-source-stream');
+
+gulp.task('build-sass', function () {
+    return gulp
+        .src("src/sass/*.sass")
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task("build-js", function () {
+    return browserify({entries: "src/scripts/Game.js", extensions: ['.js'], debug: true})
+        .transform('babelify', {presets: ['@babel/env', '@babel/react']})
+        .bundle()
+        .pipe(source("index.js"))
+        .pipe(gulp.dest("src/dist"));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(["src/sass/*.sass"], gulp.series("build-sass"));
+    gulp.watch(["src/scripts/**/*.js"], gulp.series("build-js"));
+});
+
+gulp.task('default', gulp.parallel("build-sass", "build-js", "watch"));
--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Board} from "./Board.js";
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game-space">
+                2048 eeee
+            </div>
+        )
+    }
+}
+
+
+// ===============================
+
+ReactDOM.render(
+    (<Game />),
+    document.getElementById('root')
+);
