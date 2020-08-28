8b6175e0c6deabdeff54479fc02ff9b06b1a6a8d
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-08-28 21:46:14 +0500

    (feat): Configured the return button
    Now a user can go to the previous move

--- src/scripts/Game.js
+++ src/scripts/Game.js
@@ -13,6 +13,8 @@ class Game extends React.Component {
             history: [{cells: cells}], //stores the history of moves. each state of cells stores degrees of '2'
             score: 0,
             least: 1,    //the least degree of '2' on the board
+            step_num: 0,
+            game_started: false,
         }
     }
 
@@ -27,7 +29,7 @@ class Game extends React.Component {
     renderRow(i) {
         const cells = [];
         for (let c = 0; c < 4; c++) {
-            let cell_num = i*4 + c + 1;
+            let cell_num = i*4 + c;
             cells[c] = (
                 <li key={"slot-" + cell_num}>{this.renderCell(cell_num, null, "empty")}</li>
             )
@@ -41,12 +43,13 @@ class Game extends React.Component {
     }
 
     renderFrontRow(i) {
-        const history = {}; Object.assign(history, this.state.history[this.state.history.length - 1]);
-        const cur_cells = history.cells;
+        const cur_state = {};
+        Object.assign(cur_state, this.state.history[this.state.history.length - 1]);
+        const cur_cells = cur_state.cells;
         const cells = [];
 
         for (let c = 0; c < 4; c++) {
-            let cell_num = i*4 + c + 1;
+            let cell_num = i*4 + c;
             let class_str = (cur_cells[cell_num] == null) ? "empty" : "visible value-" + cur_cells[cell_num];
             cells[c] = (
                 <li key={"cell-" + cell_num}>
@@ -64,8 +67,9 @@ class Game extends React.Component {
 
     findEmptySlot() {
         const empty_slots = [];
-        const history = this.state.history[this.state.history.length - 1]; //take the current board state
-        const cur_cells = history.cells;
+        const cur_state = {};
+        Object.assign(cur_state, this.state.history[this.state.history.length - 1]);
+        const cur_cells = cur_state.cells;
 
         let cur_ind = 0;
         for(let ind = 0; ind < cur_cells.length; ind++) {
@@ -86,21 +90,81 @@ class Game extends React.Component {
 
     spawnCell() {
         const slot_id = this.findEmptySlot();
-        const history = {}; //take the current board state
-        Object.assign(history, this.state.history[this.state.history.length - 1]);
-        const cur_cells = history.cells;
-        let least_num = 0;
+        const cur_state = {};
+        Object.assign(cur_state, this.state.history[this.state.history.length - 1]);
+        const cur_cells = cur_state.cells;
 
-        for(let c = 0; c < cur_cells.length; c++) {
-            least_num = (least_num < +cur_cells[c]) ? least_num : cur_cells[c];
+        //Placing the least number on the empty slot
+
+        cur_cells[slot_id] = Math.pow(2, this.state.least);
+        this.setState({
+            history: this.state.history.concat([{cells: cur_cells}])
+        });
+    }
+
+    startBtnContent(game_started) {
+        if(game_started) {
+            return (
+                <button id="newGame"
+                        onClick={() => this.startGame()}>
+                    <svg width="16px" height="16px" viewBox="0 0 16 16" className="button-icon bi bi-arrow-repeat"
+                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
+                              d="M2.854 7.146a.5.5 0 0 0-.708 0l-2 2a.5.5 0 1 0 .708.708L2.5 8.207l1.646 1.647a.5.5 0 0 0 .708-.708l-2-2zm13-1a.5.5 0 0 0-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 0 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0 0-.708z"/>
+                        <path fillRule="evenodd"
+                              d="M8 3a4.995 4.995 0 0 0-4.192 2.273.5.5 0 0 1-.837-.546A6 6 0 0 1 14 8a.5.5 0 0 1-1.001 0 5 5 0 0 0-5-5zM2.5 7.5A.5.5 0 0 1 3 8a5 5 0 0 0 9.192 2.727.5.5 0 1 1 .837.546A6 6 0 0 1 2 8a.5.5 0 0 1 .501-.5z"/>
+                    </svg>
+                    Заново
+                </button>
+            )
+        }
+        else {
+            return (
+                <button
+                    id="newGame"
+                    onClick={() => this.startGame()}
+                >
+                    Начать
+                </button>)
+        }
+    }
+
+    startGame(clearContent) {
+        this.clearBoard();
+
+        this.setState({
+            game_started: true,
+            step_num: 1
+        }, () => {
+            this.spawnCell();
+            this.spawnCell();
+        })
+    }
+
+    clearBoard() {
+        this.setState({
+            history: [{cells: Array(16).fill(null)}], //stores the history of moves. each state of cells stores degrees of '2'
+            score: 0,
+            least: 1,    //the least degree of '2' on the board
+            step_num: 0,
+            game_started: false,
+        })
+    }
+
+    goBack() {
+        const moves = this.state.step_num;
+
+        if(moves <= 1) {
+            return;
         }
 
-
+        this.setState({
+            history: this.state.history.slice(0, this.state.history.length - 1),
+            step_num: moves - 1,
+        })
     }
 
     render() {
-
-
         return (
             <div className="game-space">
                 <div className="game-panel">
@@ -109,7 +173,9 @@ class Game extends React.Component {
                         <span id="score">{this.state.score}</span>
                     </div>
                     <div className="buttons-block">
-                        <button id="goBack">
+                        <button id="goBack"
+                                onClick={() => this.goBack()}
+                        >
                             <svg width="16px" height="16px" viewBox="0 0 16 16" className="button-icon bi bi-arrow-counterclockwise"
                                  fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd"
@@ -119,16 +185,7 @@ class Game extends React.Component {
                             </svg>
                             Назад
                         </button>
-                        <button id="newGame">
-                            <svg width="16px" height="16px" viewBox="0 0 16 16" className="button-icon bi bi-arrow-repeat"
-                                 fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fillRule="evenodd"
-                                      d="M2.854 7.146a.5.5 0 0 0-.708 0l-2 2a.5.5 0 1 0 .708.708L2.5 8.207l1.646 1.647a.5.5 0 0 0 .708-.708l-2-2zm13-1a.5.5 0 0 0-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 0 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0 0-.708z"/>
-                                <path fillRule="evenodd"
-                                      d="M8 3a4.995 4.995 0 0 0-4.192 2.273.5.5 0 0 1-.837-.546A6 6 0 0 1 14 8a.5.5 0 0 1-1.001 0 5 5 0 0 0-5-5zM2.5 7.5A.5.5 0 0 1 3 8a5 5 0 0 0 9.192 2.727.5.5 0 1 1 .837.546A6 6 0 0 1 2 8a.5.5 0 0 1 .501-.5z"/>
-                            </svg>
-                            Заново
-                        </button>
+                        {this.startBtnContent(this.state.game_started)}
                     </div>
                 </div>
                 <Board
