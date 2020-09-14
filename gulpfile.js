876540b8ed40a1a492335f453f86eef9e0c26711
Ivan Gladkikh <ivangladkikh3@gmail.com>
2020-09-14 13:37:09 +0500

    (feat): Created a build task in gulp
    And built the source files into public
    directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
-      prefix = require('gulp-autoprefixer')/*,
+      prefix = require('gulp-autoprefixer'),
       browserify = require('browserify'),
       babelify = require('babelify'),
-      source = require('vinyl-source-stream')*/;
+      source = require('vinyl-source-stream');
 
-gulp.task('build-sass', function () {
+gulp.task('gulp-sass', function () {
     return gulp
         .src("src/sass/*.sass")
         .pipe(sass())
@@ -13,17 +13,43 @@ gulp.task('build-sass', function () {
         .pipe(gulp.dest('src/css/'));
 });
 
-/*gulp.task("build-js", function () {
-    return browserify({entries: "src/scripts/Game.js", extensions: ['.js'], debug: true})
-        .transform('babelify', {presets: ['@babel/env', '@babel/react']})
+gulp.task("bundle-js", function () {
+    return browserify({entries: "src/index.js", extensions: ['.js'], debug: true})
+        .transform('babelify', {presets: ['@babel/preset-env', '@babel/preset-react']})
         .bundle()
         .pipe(source("index.js"))
         .pipe(gulp.dest("src/dist"));
-});*/
-
-gulp.task('watch', function () {
-    gulp.watch(["src/sass/*.sass"], gulp.series("build-sass"));
-    //gulp.watch(["src/scripts/**/*.js"], gulp.series("build-js"));
 });
 
-gulp.task('default', gulp.parallel("build-sass", /*"build-js",*/ "watch"));
+/*__________ BUILD _____________________________*/
+
+gulp.task("build-html", function () {
+    return gulp.src("src/index.html")
+        .pipe(gulp.dest("public/"));
+});
+
+gulp.task("build-js", function () {
+    return gulp.src("src/dist/index.js")
+        .pipe(gulp.dest("public/dist/"));
+});
+
+gulp.task("build-css", function () {
+    return gulp.src("src/css/*.css")
+        .pipe(gulp.dest("public/css/"));
+});
+
+gulp.task('build', gulp.series(
+    ["gulp-sass",
+    "bundle-js",
+    "build-js",
+    "build-css",
+    "build-html"]
+));
+
+/*______________________________________*/
+
+gulp.task('watch', function () {
+    gulp.watch(["src/sass/*.sass"], gulp.series("gulp-sass"));
+});
+
+gulp.task('default', gulp.parallel("gulp-sass", "watch"));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import {Game} from "./scripts/Game.js"
-import "./css/main.css"
 
 ReactDOM.render(
     (<Game />),
